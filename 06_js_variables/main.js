41f0c2a0028c35ab8a68b7de9d72aa46248c3546
elsa-ve <e.ventruba@hotmail.com>
2020-03-24 16:52:44 +0100

    String Concatenation with Variables

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,10 +1,2 @@
-let gainedDollar = 3;
-let lostDollar = 50;
-gainedDollar++;
-lostDollar--;
-/*Other mathematical assignment operators include the increment operator (++) and decrement operator (--).
-
-The increment operator will increase the value of the variable by 1. 
-The decrement operator will decrease the value of the variable by 1.
-Just like the previous mathematical assignment operators (+=, -=, *=, /=), 
-the variable’s value is updated and assigned as the new value of that variable.*/
+let favoriteAnimal = 'penguin';
+console.log('My favorite animal' + ': ' + favoriteAnimal);
