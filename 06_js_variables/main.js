95cfe5f9bbefe9f5e0477b8bd3c1b9c5477635cf
elsa-ve <e.ventruba@hotmail.com>
2020-03-24 17:03:55 +0100

    template literals

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,3 +1,5 @@
 let myName = 'Elsa';
 let myCity = 'Vienna';
 console.log (`My name is ${myName}. My favorite city is ${myCity}`);
+/*a template literal is wrapped by backticks ` (this key is usually located on the top of your keyboard, left of the 1 key).
+Inside the template literal, you’ll see a placeholder, ${myPet}. The value of myPet is inserted into the template literal.*/
