36c33fdfc8dd4acee2f4b9a93b8665a3dcb632c7
elsa-ve <e.ventruba@hotmail.com>
2020-03-24 16:38:54 +0100

    mathematical assignment operators

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,9 +1,18 @@
-const entree = 'Enchiladas';
-console.log(entree);
-entree = 'Tacos';
-const testing;
-/*a const variable cannot be reassigned because it is constant.
-Constant variables must be assigned a value when declared.
-If you’re trying to decide between which keyword to use, let or const, 
-think about whether you’ll need to reassign the variable later on. 
-If you do need to reassign the variable use let, otherwise, use const. */
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp +=5;
+powerLevel -=100;
+multiplyMe *=11;
+quarterMe /=4;
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
