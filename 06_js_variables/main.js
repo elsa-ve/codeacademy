4ca2c32da84381c9acdf8c8104b6919c2b2f68f6
elsa-ve <e.ventruba@hotmail.com>
2020-03-24 16:34:18 +0100

    Create a Variable: const

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,2 +1,9 @@
-let changeMe = false;
-console.log(changeMe);
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos';
+const testing;
+/*a const variable cannot be reassigned because it is constant.
+Constant variables must be assigned a value when declared.
+If you’re trying to decide between which keyword to use, let or const, 
+think about whether you’ll need to reassign the variable later on. 
+If you do need to reassign the variable use let, otherwise, use const. */
