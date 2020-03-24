a5340fd47c88766df6c29dd16256a685554b8f4c
elsa-ve <e.ventruba@hotmail.com>
2020-03-24 16:57:36 +0100

    String Interpolation

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,2 +1,3 @@
-let favoriteAnimal = 'penguin';
-console.log('My favorite animal' + ': ' + favoriteAnimal);
+let myName = 'Elsa';
+let myCity = 'Vienna';
+console.log (`My name is ${myName}. My favorite city is ${myCity}`);
